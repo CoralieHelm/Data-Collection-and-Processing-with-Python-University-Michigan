94585b5bb74f78d152c8c17f85d900681270fbfe
CoralieHelm <helm.coralie@gmail.com>
2020-06-12 13:33:09 -0400

    Added additional exericses to Lists with Complex Items.

--- 17_1_1_Lists_with_Complex_Items.py
+++ 17_1_1_Lists_with_Complex_Items.py
@@ -15,3 +15,35 @@ data = ['bagel', 'cream cheese', 'breakfast', 'grits', 'eggs', 'bacon', [34, 9, 
 
 plant = data[7][0][0]
 print(plant)
+
+#Check Your Understanding
+
+nested-2-1: Which of the following is a legal assignment statement, after the following code executes?
+
+d = {'key1': {'a': 5, 'c': 90, 5: 50}, 'key2':{'b': 3, 'c': "yes"}}
+#A. d[5] = {1: 2, 3: 4}
+#B. d[{1:2, 3:4}] = 5
+#C. d['key1']['d'] = d['key2']
+#D. d[key2] = 3
+
+#A & C
+
+#1. Extract the value associated with the key color and assign it to the variable color. Do not hard code this.
+
+info = {'personal_data':
+         {'name': 'Lauren',
+          'age': 20,
+          'major': 'Information Science',
+          'physical_features':
+             {'color': {'eye': 'blue',
+                        'hair': 'brown'},
+              'height': "5'8"}
+         },
+       'other':
+         {'favorite_colors': ['purple', 'green', 'blue'],
+          'interested_in': ['social media', 'intellectual property', 'copyright', 'music', 'books']
+         }
+      }
+
+color = info["personal_data"]["physical_features"]["color"]
+print(color)
